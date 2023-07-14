1becb506b28e77520e7e226ef78d509dd466dbda
GaneshJoshi117 <ganeshjoshi583@gmail.com>
2023-07-14 17:27:51 +0530

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: [true, 'Email already in exists'],
+		required: [true, 'Email is required'],
+	},
+	username: {
+		type: String,
+		required: [true, 'Username is required'],
+		match: [
+			/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+			'Username invalid, it should contain 8-20 alphanumeric letters and be unique!',
+		],
+	},
+	image: {
+		type: String,
+	},
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
